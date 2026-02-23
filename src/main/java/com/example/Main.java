0350ecb81c91d34685beb64b951eb6e16b3af581
harshanacz <harshanaamuwatte@gmail.com.(none)>
2026-02-23 17:29:30 +0530

    build failed issue fixed

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import org.teavm.jso.JSBody;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from WASM!");
+        consoleLog("Hello from WASM!");
         String result = add(10, 20);
-        System.out.println("10 + 20 = " + result);
+        consoleLog("10 + 20 = " + result);
     }
+
+    @JSBody(params = {"message"}, script = "console.log(message);")
+    private static native void consoleLog(String message);
 
     static String add(int a, int b) {
         return String.valueOf(a + b);
