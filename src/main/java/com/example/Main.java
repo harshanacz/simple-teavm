924efd9b247eabb0c9dbf6b2e1400bd245c2f8ad
harshanacz <harshanaamuwatte@gmail.com.(none)>
2026-02-23 22:47:32 +0530

    feat: Add a basic TeaVM project compiling Java to WebAssembly with an HTML runner.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,11 @@
 package com.example;
 
-import org.teavm.jso.JSBody;
-
 public class Main {
     public static void main(String[] args) {
-        consoleLog("Hello from WASM!");
+        System.out.println("Hello from WASM!");
         String result = add(10, 20);
-        consoleLog("10 + 20 = " + result);
+        System.out.println("10 + 20 = " + result);
     }
-
-    @JSBody(params = {"message"}, script = "console.log(message);")
-    private static native void consoleLog(String message);
 
     static String add(int a, int b) {
         return String.valueOf(a + b);
